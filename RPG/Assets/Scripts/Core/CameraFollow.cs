df1843b79d73f1fe4d5251ce66c9c34ccd1d44ad
DESKTOP-NGBNHNQ\Michał <mwarioxin@gmail.com>
2019-05-13 22:04:48 +0200

    Finished adding animation to movment, added blend trees

---
+++ RPG/Assets/Scripts/Core/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    private Vector3 positionDifference;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var diferpositionDifferenceence = target.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = target.position + positionDifference;
+    }
+}
---
+++ RPG/Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField]
+    public Transform target;
+
+    Ray lastRay;
+    private NavMeshAgent navMesh;
+
+ 
+
+    void Start()
+    {
+        navMesh = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            MoveToCursor(Input.mousePosition);
+
+        }
+
+        UpdateAnimator();
+        
+    }
+
+    void MoveToCursor(Vector3 targetPosition)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(targetPosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit)
+        {
+            navMesh.destination = hit.point;
+        }
+
+    }
+
+    private void UpdateAnimator()
+    {
+        //! Parse navmesh velocity to animator blend tree to adjust animation speed of player
+        Vector3 velocity = navMesh.velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity); //change global velocity to local
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed); //change parameter in animator parameters to change animation in blend tree
+
+    }
+
+}
